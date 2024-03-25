f397fecaf13cf03e6ca34ef4b0e45576391e3256
Halil <halilibrahimkasikci@hotmail.com>
2024-03-26 00:55:31 +0300

    Commit

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import util.Commands;
@@ -14,12 +16,26 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
-        chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
-        driver = new ChromeDriver(chromeOptions);
         Config config = new Config();
+        String browser = Config.get("browser");
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--start-maximized");
+                chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--start-maximized");
+                edgeOptions.addArguments("--disable-blink-features=AutomationControlled");
+                driver = new EdgeDriver(edgeOptions);
+                break;
+            default:
+                throw new IllegalArgumentException("Geçersiz tarayıcı: " + browser);
+        }
         Commands commands = new Commands(driver);
     }
 
